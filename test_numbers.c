2795d319bd11abfd63bad48b4d365055100dec6c
Troels Ramsey <94567632+troelsramsey@users.noreply.github.com>
2021-12-02 11:05:20 +0100

    Delete test_numbers.c

--- test_numbers.c
+++
@@ -1,7 +0,0 @@
-#include <stdlib.h>
-#include "numbers.h"
-
-int main() {
-  struct bits8 test = bits8_from_int(460);
-  printf("result: %d%d%d%d%d%d%d%d\n", test.b7.v, test.b6.v, test.b5.v, test.b4.v, test.b3.v, test.b2.v, test.b1.v, test.b0.v);
-}
